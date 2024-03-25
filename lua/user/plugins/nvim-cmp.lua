ed8ffcac59e32bc93b344cc343611e5344715a57
RchrdAriza <rchrdalv@gmail.com>
2024-03-25 12:58:52 -0500

    Disabling completion in certain contexts, such as comments

--- lua/user/plugins/nvim-cmp.lua
+++ lua/user/plugins/nvim-cmp.lua
@@ -54,7 +54,17 @@ return {
 
 			cmp.setup({
 				-- experimental = { ghost_text = true },
-				--
+				enabled = function()
+					-- disable completion in comments
+					local context = require("cmp.config.context")
+					-- keep command mode completion enabled when cursor is in a comment
+					if vim.api.nvim_get_mode().mode == "c" then
+						return true
+					else
+						return not context.in_treesitter_capture("comment") and not context.in_syntax_group("Comment")
+					end
+				end,
+
 				view = {
 					entries = { name = "custom", selection_order = "near_cursor" },
 				},
