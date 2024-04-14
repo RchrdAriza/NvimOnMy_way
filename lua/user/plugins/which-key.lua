7f6387c1044891d2482694f3a7db34ee2616b0ab
RchrdAriza <rchrdalv@gmail.com>
2024-04-13 22:26:09 -0500

    modified README

--- lua/user/plugins/which-key.lua
+++ lua/user/plugins/which-key.lua
@@ -69,7 +69,7 @@ return {
 				e = { "<cmd>:lua vim.diagnostic.open_float()<cr>", "vim.diagnostic" },
 				t = { "<cmd>:lua vim.lsp.buf.type_definition()<cr>", "lsp.buf.type_definition" },
 				R = { "<cmd>:lua vim.lsp.buf.references()<cr>", "lsp.buf.references" },
-				s = { "<cmd>:lua vim.lsp.buf.signature_help()<cr>", "lsp.buf.signature_help" },
+				s = { "<cmd>:lua vim.lsp.signature_help()<cr>", "lsp.buf.references" },
 			},
 		})
 
