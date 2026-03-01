672b482848823bee0459adebbd26225182b4a66f
RchrdAriza <rchrdalv@gmail.com>
2026-03-01 18:40:28 -0500

    refactor which-key: move all keymaps to wk.add() with icons
    - All
    - All keymaps now in wk.add() instead of lazy keys (icons not supported
    there)
    - Fix duplicate <leader>tb, wrong lsp signature fn, and <leader>x
    conflict
    - Add nerd font icons to all groups and mappings

--- lua/user/plugins/which-key.lua
+++ lua/user/plugins/which-key.lua
@@ -1,58 +1,56 @@
 return {
-	"folke/which-key.nvim",
-	event = "VeryLazy",
-	opts = {
-		preset = "modern",
-	},
-	config = function()
-		local wk = require("which-key")
-		wk.add({
-			{ "<leader>t", group = "telescope", icon = "" },
-			{ "<leader>tf", "<cmd>Telescope find_files<cr>", desc = "Find file", mode = "n" },
-			{ "<leader>tg", "<cmd>Telescope live_grep<cr>", desc = "live_grep" },
-			{ "<cmd>Telescope live_grep<cr>", desc = "live_grep" },
-			{ "<leader>tb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
-			{ "<leader>tb", "<cmd>Telescope help_tags<cr>", desc = "Help_tags" },
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    config = function()
+        local wk = require("which-key")
 
-			-------------------
-			{ "<leader>l", group = "Lsp Actions", icon = "" },
-			{ "<leader>lD", "<cmd>:lua vim.lsp.buf.declaration()<cr>", desc = "lsp.buf.declaration" },
-			{ "<leader>ld", "<cmd>:lua vim.lsp.buf.definition()<cr>", desc = "vim.lsp.buf.definition" },
-			{ "<leader>lh", "<cmd>:lua vim.lsp.buf.hover()<cr>", desc = "lsp.buf.hover" },
-			{ "<leader>li", "<cmd>:lua vim.lsp.buf.implementation()<cr>", desc = "lsp.buf.implementation" },
-			{ "<leader>lr", "<cmd>:lua vim.lsp.buf.rename()<cr>", desc = "lsp.buf.rename" },
-			{ "<leader>la", "<cmd>:lua vim.lsp.buf.code_action()<cr>", desc = "lsp.buf.code_action" },
-			{ "<leader>le", "<cmd>:lua vim.diagnostic.open_float()<cr>", desc = "vim.diagnostic" },
-			{ "<leader>lt", "<cmd>:lua vim.lsp.buf.type_definition()<cr>", desc = "lsp.buf.type_definition" },
-			{ "<leader>lR", "<cmd>:lua vim.lsp.buf.references()<cr>", desc = "lsp.buf.references" },
-			{ "<leader>ls", "<cmd>:lua vim.lsp.signature_help()<cr>", desc = "lsp.buf.references" },
+        wk.setup({ preset = "modern" })
 
-			---------------------
-			{ "<leader>g", group = "Git" },
-			{ "<leader>ga", "<cmd>Gwrite<cr>", desc = "Git Add [this file]" },
-			{ "<leader>gs", "<cmd>Neotree float git_status<cr>", desc = "Git Status" },
-			{ "<leader>gc", "<cmd>Git commit<cr>", desc = "Git Commit" },
-			{ "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Git Diff" },
-			---------------------
+        wk.add({
+            -- Grupos y keymaps CON icono van aquí
+            { "<leader>t",  group = "Telescope",   icon = { icon = "", color = "blue" } },
+            { "<leader>tf", "<cmd>Telescope find_files<cr>",  desc = "Find files",   icon = "󰈞" },
+            { "<leader>tg", "<cmd>Telescope live_grep<cr>",   desc = "Live grep",    icon = "󰈈" },
+            { "<leader>tb", "<cmd>Telescope buffers<cr>",     desc = "Buffers",      icon = "󰕬" },
+            { "<leader>th", "<cmd>Telescope help_tags<cr>",   desc = "Help tags",    icon = "" },
 
-			{ "<leader>x", group = "Terminal", icon = "" },
-			{ "<Space>xh", "<cmd>ToggleTerm size=10 direction=horizontal<cr>", desc = "Horizontal term" },
-			{ "<leader>xf", "<cmd>ToggleTerm direction=float<cr>", desc = "Floating terminal" },
-			{ "<leader>xt", "<cmd>ToggleTerm direction=tab<cr>", desc = "Tab terminal" },
-			{ "<leader>xv", "<cmd>ToggleTerm size=50 direction=vertical<cr>", desc = "Vertical term" },
-		})
-	end,
-	keys = {
-		{ "<leader><Space>", "<cmd>noh<cr>", desc = "Noh" },
-		{ "<leader>e", "<cmd>Neotree<cr>", desc = "File Explorer" },
-		{ "<leader>n", "<cmd>enew<cr>", desc = "New file" },
-		{ "<leader>r", "<cmd>Telescope oldfiles<cr>", desc = "Open recent files" },
-		{ "<leader>h", "<cmd>Alpha<cr>", desc = "home" },
-		{ "<leader>c", "<cmd>:bwipeout<cr>", desc = "Close tab ()" },
-		{ "<leader>q", "<cmd>q!<cr>", desc = "Exit" },
-		{ "<leader>x", "<cmd>x<cr>", desc = "Exit and save" },
-		{ "<leader>R", "<cmd>RunCode<cr>", desc = "Run Code" },
-		{ "<leader>d", "<cmd>TroubleToggle<cr>", desc = "See all errors" },
-		{ "<leader>a", "<cmd>ASToggle<cr>", desc = "AutoSave On/Off" },
-	},
+            { "<leader>l",  group = "LSP Actions", icon = { icon = "", color = "cyan" } },
+            { "<leader>lD", vim.lsp.buf.declaration,       desc = "Declaration",    icon = "" },
+            { "<leader>ld", vim.lsp.buf.definition,        desc = "Definition",     icon = "󰊕" },
+            { "<leader>lh", vim.lsp.buf.hover,             desc = "Hover",          icon = "" },
+            { "<leader>li", vim.lsp.buf.implementation,    desc = "Implementation", icon = "" },
+            { "<leader>lr", vim.lsp.buf.rename,            desc = "Rename",         icon = "󰑕" },
+            { "<leader>la", vim.lsp.buf.code_action,       desc = "Code action",    icon = "" },
+            { "<leader>le", vim.diagnostic.open_float,     desc = "Diagnostics",    icon = "" },
+            { "<leader>lt", vim.lsp.buf.type_definition,   desc = "Type definition",icon = "" },
+            { "<leader>lR", vim.lsp.buf.references,        desc = "References",     icon = "" },
+            { "<leader>ls", vim.lsp.buf.signature_help,    desc = "Signature help", icon = "" },
+
+            { "<leader>g",  group = "Git",         icon = { icon = "", color = "orange" } },
+            { "<leader>ga", "<cmd>Gwrite<cr>",             desc = "Git add",        icon = "" },
+            { "<leader>gs", "<cmd>Neotree float git_status<cr>", desc = "Git status", icon = "" },
+            { "<leader>gc", "<cmd>Git commit<cr>",         desc = "Git commit",     icon = "" },
+            { "<leader>gd", "<cmd>DiffviewOpen<cr>",       desc = "Git diff",       icon = "" },
+
+            { "<leader>T",  group = "Terminal",    icon = { icon = "", color = "green" } },
+            { "<leader>Th", "<cmd>ToggleTerm size=10 direction=horizontal<cr>", desc = "Horizontal", icon = "" },
+            { "<leader>Tf", "<cmd>ToggleTerm direction=float<cr>",              desc = "Float",      icon = "󰉈" },
+            { "<leader>Tt", "<cmd>ToggleTerm direction=tab<cr>",                desc = "Tab",        icon = "󰓩" },
+            { "<leader>Tv", "<cmd>ToggleTerm size=50 direction=vertical<cr>",   desc = "Vertical",   icon = "" },
+
+            -- Keymaps generales también aquí para tener iconos
+            { "<leader><Space>", "<cmd>noh<cr>",               desc = "Clear highlights", icon = "󰸱" },
+            { "<leader>e",       "<cmd>Neotree<cr>",            desc = "File explorer",    icon = "" },
+            { "<leader>n",       "<cmd>enew<cr>",               desc = "New file",         icon = "" },
+            { "<leader>r",       "<cmd>Telescope oldfiles<cr>", desc = "Recent files",     icon = "󰙰" },
+            { "<leader>h",       "<cmd>Alpha<cr>",              desc = "Home",             icon = "󰋜" },
+            { "<leader>c",       "<cmd>bwipeout<cr>",           desc = "Close buffer",     icon={icon = "", color="red"} },
+            { "<leader>q",       "<cmd>q!<cr>",                 desc = "Quit",             icon = "󰩈" },
+            { "<leader>w",       "<cmd>w<cr>",                  desc = "Save",             icon = "󰆓" },
+            { "<leader>x",       "<cmd>x<cr>",                  desc = "Save & quit",      icon = "󰗼" },
+            { "<leader>R",       "<cmd>RunCode<cr>",            desc = "Run code",         icon = "" },
+            { "<leader>d",       "<cmd>TroubleToggle<cr>",      desc = "Diagnostics",      icon = "" },
+            { "<leader>a",       "<cmd>ASToggle<cr>",           desc = "AutoSave toggle",  icon = "󰿁" },
+        })
+    end,
 }
