cc4d78c194bcacf0ecef8fb95665ea59f8ed1127
RchrdAriza <rchrdalv@gmail.com>
2026-03-02 00:36:29 -0500

    I deleted some unnecessary files and unified some imports

--- lua/user/plugins/gitsign.lua
+++
@@ -1,4 +0,0 @@
-return {
-	{ "lewis6991/gitsigns.nvim", opts = {} },
-	{ "akinsho/git-conflict.nvim", version = "*", config = true },
-}
--- lua/user/plugins/terminal.lua
+++ lua/user/plugins/terminal.lua
@@ -1,6 +1,3 @@
 return {
-	-- amongst your other plugins
 	{ "akinsho/toggleterm.nvim", version = "*", config = true },
-	-- -- or
-	-- {'akinsho/toggleterm.nvim', version = "*", opts = {--[[ things you want to change go here]]}}
 }
