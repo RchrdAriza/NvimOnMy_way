784f41e6cafadcf78a3cfe0cfc37ff4d1aba5644
RchrdAriza <rchrdalv@gmail.com>
2026-06-15 09:19:53 -0500

    resolví conflicto entre noise y nvim.notify

--- lua/user/plugins/ui.lua
+++ lua/user/plugins/ui.lua
@@ -672,7 +672,9 @@ return {
 			"folke/noice.nvim",
 			event = "VeryLazy",
 			opts = {
-				-- add any options here
+				notify = {
+					enabled = false,
+				}
 			},
 			dependencies = {
 				-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
